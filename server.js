1d10cbb32d2469e9001919e6a5572f51197a07e2
Sebastian Odell <sebastianodell@gmail.com>
2015-11-27 10:18:35 -0800

    getting data from API server side

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 		PORT    = process.env.PORT || 3000,
 		dotenv	= require('dotenv'),
+		request = require('request'),
 		server  = express();
 
 dotenv.load();
@@ -12,17 +13,20 @@ server.use('/bower_components', express.static('./bower_components'));
 
 server.use(express.static(__dirname + '/public'));
 
+//This gets artist data
 server.get('/explore/:artist', function(req, res){
-	console.log(req.params.artist)
+	var ARTIST = req.params.artist;
+	request('http://developer.echonest.com/api/v4/artist/profile?api_key=' + process.env.EN_AK + '&name=' + ARTIST + '&bucket=biographies&bucket=blogs&bucket=images', function(err, res, bod){
+		console.log(bod)
+	})
 })
 
 //This delegates all front-end routing to angular
 server.get('*', function(req, res) {
-            res.sendFile(__dirname + '/public/' + 'index.html'); // load our public/index.html file
-        });
+  res.sendFile(__dirname + '/public/' + 'index.html'); // load our public/index.html file
+});
 
 //run server
 server.listen(PORT, function(){
 	console.log('listening on port ' + PORT)
 });
-
