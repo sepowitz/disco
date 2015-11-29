7222dd11db20d46f28bc5714d803d815d74372bb
Sebastian Odell <sebastianodell@gmail.com>
2015-11-28 17:32:14 -0800

    set up API call for artist by country of origin

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ server.use(express.static(__dirname + '/public'));
 //This gets artist data
 server.get('/explore/:artist', function(req, res){
 	var ARTIST = req.params.artist;
-	request('http://developer.echonest.com/api/v4/artist/profile?api_key=' + process.env.EN_AK + '&name=' + ARTIST + '&bucket=biographies&bucket=blogs&bucket=images', function(error, response, body){
+	// request('http://developer.echonest.com/api/v4/artist/profile?api_key=' + process.env.EN_AK + '&name=' + ARTIST + '&bucket=biographies&bucket=blogs&bucket=images&bucket=id:spotify', function(error, response, body){
+		request('http://developer.echonest.com/api/v4/artist/search?api_key=' + process.env.EN_AK + '&format=json&artist_location=country:' + ARTIST + '&bucket=artist_location', function(error, response, body){
 		res.send(body)
 	})
 })
