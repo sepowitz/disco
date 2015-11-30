0bc66bf078c7622cc8ebd8a374bd4ec16a9f70bb
Sebastian Odell <sebastianodell@gmail.com>
2015-11-29 20:34:43 -0500

    added bootstrap classes for styling foundation

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,13 +1,13 @@
-angular.module('DiscoGrid').config( function($routeProvider, $locationProvider) {
-	$routeProvider.
-	when('/explore/:artist', {
-		templateUrl: '/app/components/artist.html',
-		controller: 'ArtistController',
-		controllerAs: 'artistCtrl'
-	}).
-	otherwise({	
-		redirectTo: '/explore'
-	});
+// angular.module('DiscoGrid').config( function($routeProvider, $locationProvider) {
+// 	$routeProvider.
+// 	when('/explore/:artist', {
+// 		templateUrl: '/app/components/artist.html',
+// 		controller: 'ArtistController',
+// 		controllerAs: 'artistCtrl'
+// 	}).
+// 	otherwise({	
+// 		redirectTo: '/explore'
+// 	});
 
-	$locationProvider.html5Mode(true);
-});
+// 	$locationProvider.html5Mode(true);
+// });
