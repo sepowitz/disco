54f24e3be72c0c67d30daac29df1e3286e15c26e
Sebastian Odell <sebastianodell@gmail.com>
2015-11-29 19:13:22 -0500

    pulling relevant artist data, searched by country of origin

--- public/app/controllers/explore_controller.js
+++ public/app/controllers/explore_controller.js
@@ -1,10 +1,23 @@
 angular.module('DiscoGrid').controller('ExploreController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 	var explore = this;
-			explore.playlist = [];
+			explore.error = false;
+			explore.errorMessage = "Whoops! Looks like there was an issue, try again!"
 			explore.searchCountry = function(country){
 				$http.get('/explore/' + country ).success(function(data){
 					explore.data = data;
 					console.log(explore.data)
+					if(explore.data.name) {
+						explore.error = false;
+						for(var i = 0; i < explore.data.biographies.length; i++){
+						if(explore.data.biographies[i].truncated !== true){
+							explore.bio = explore.data.biographies[i];
+							console.log(explore.bio)
+						}
+					}
+				} else {
+					explore.error = true;
+				}
+
 				})
 			}
-}]);
+}]); 
--- server.js
+++ server.js
@@ -17,12 +17,17 @@ server.use(express.static(__dirname + '/public'));
 server.get('/explore/:country', function(req, res){
 	var COUNTRY = req.params.country;
 		//Request artist by country
-		request('http://developer.echonest.com/api/v4/artist/search?api_key=' + process.env.EN_AK + '&format=json&artist_location=country:' + COUNTRY + '&bucket=artist_location&bucket=biographies&bucket=id:spotify', function(error, response, body){
-		//Parse response in order to extract artist name	
+		request('http://developer.echonest.com/api/v4/artist/search?api_key=' + process.env.EN_AK + '&format=json&artist_location=country:' + COUNTRY + '&bucket=artist_location&bucket=biographies&bucket=blogs&bucket=video&bucket=id:spotify', function(error, response, body){
+		//Parse response in order to extract artist name
+		console.log(body)	
 		var parsedRes = JSON.parse(body);
-		var ARTIST = (parsedRes.response.artists[Math.floor(Math.random() * parsedRes.response.artists.length)])
-		//Make subsequent call for select artists' related tracks
-		res.send(ARTIST)
+		if(parsedRes.response.artists < 1){
+			res.send('Oops, there was a problem')
+		} else {
+			var ARTIST = (parsedRes.response.artists[Math.floor(Math.random() * parsedRes.response.artists.length)])
+			//Make subsequent call for select artists' related tracks
+			res.send(ARTIST)
+		}
 	})
 })
 
