809b1c6cc49dc29a8f3ce5a4fe3a610c8d2c03fa
Sebastian Odell <sebastianodell@gmail.com>
2015-12-01 08:21:59 -0500

    added additional styling and favicon

--- public/app/controllers/explore_controller.js
+++ public/app/controllers/explore_controller.js
@@ -1,17 +1,20 @@
 angular.module('DiscoGrid').controller('ExploreController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 	var explore = this;
 			explore.error = false;
+			explore.loading = false;
 			explore.errorMessage = "Whoops, looks like there was an issue. Give it another spin."
 			explore.searchCountry = function(country){
+				explore.loading = true;
 				$http.get('/explore/' + country ).success(function(data){
 					explore.data = data;
+					console.log(explore.data)
 					console.log(explore.data)
 					if(explore.data.name) {
 						explore.error = false;
 						for(var i = 0; i < explore.data.biographies.length; i++){
 						if(explore.data.biographies[i].truncated !== true){
 							explore.bio = explore.data.biographies[i];
-							console.log(explore.bio)
+							explore.loading = false;
 						}
 					}
 				} else {
